c29a67d11ebfaa2458e465cd8ba8f7ff2846cff6
Jesper Stemann Andersen <jsa@ihpostal.dk>
2015-07-28 16:28:44 +0200

    Added build script using BinDeps.

---
+++ deps/build.jl
@@ -0,0 +1,27 @@
+using BinDeps
+
+@BinDeps.setup
+
+const version = "0.9.20"
+
+libvl = library_dependency("libvl", aliases = ["vl"])
+
+provides(Sources, URI("http://www.vlfeat.org/download/vlfeat-$version.tar.gz"),	libvl)
+
+src = joinpath(srcdir(libvl), "vlfeat-$version")
+usr = usrdir(libvl)
+shlib_ext = BinDeps.shlib_ext
+arch = OS_NAME == :Linux ? "glnxa64" : OS_NAME == :Darwin ? "maci64" : "*"
+provides(BuildProcess,
+	(@build_steps begin
+		GetSources(libvl)
+		@build_steps begin
+			FileRule(joinpath(usr, "lib", "libvl."*shlib_ext), @build_steps begin
+				MakeTargets(src, ["all"]; env = Dict("MEX" => ""))
+				CreateDirectory(libdir(libvl))
+				`cp -a $src/bin/$arch/libvl.$shlib_ext $usr/lib`
+			end)
+		end
+	end), libvl, os = :Unix)
+
+@BinDeps.install Dict(:libvl => :libvl)
