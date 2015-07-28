03d272560abe2639fe2712a4e2f0054744e0acd3
Jesper Stemann Andersen <jsa@ihpostal.dk>
2015-07-28 18:26:00 +0200

    Updated to use libvl shared library built by included build script.

--- src/VLFeat.jl
+++ src/VLFeat.jl
@@ -1,9 +1,15 @@
 module VLFeat
 using Images
+using BinDeps
+
+depsfile = Pkg.dir("VLFeat","deps","deps.jl")
+if isfile(depsfile)
+    include(depsfile)
+else
+    error("VLFeat not properly installed. Please run Pkg.build(\"VLFeat\")")
+end
 
 export vl_hog
-
-const libvl=:libvl
 
 include("libvl/LIBVL.jl")
 include("hog.jl")
