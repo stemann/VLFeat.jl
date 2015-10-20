799abd8e945c1b30a3add3255c88f3c1341800bd
Marco Fraccaro <mf@ihpostal.dk>
2015-10-20 12:23:43 +0200

    Windows build

--- deps/build.jl
+++ deps/build.jl
@@ -7,22 +7,37 @@ const version = "0.9.20"
 
 libvl = library_dependency("libvl", aliases = ["vl"])
 
-provides(Sources, URI("http://www.vlfeat.org/download/vlfeat-$version.tar.gz"),	libvl)
+provides(Sources, URI("http://www.vlfeat.org/download/vlfeat-$version-bin.tar.gz"),	libvl, unpacked_dir="vlfeat-$version", os = :Windows)
+provides(Sources, URI("http://www.vlfeat.org/download/vlfeat-$version.tar.gz"),	libvl, os = :Unix)
 
 src = joinpath(srcdir(libvl), "vlfeat-$version")
 usr = usrdir(libvl)
 shlib_ext = BinDeps.shlib_ext
+
 arch = OS_NAME == :Linux ? "glnxa64" : OS_NAME == :Darwin ? "maci64" : "*"
+
 provides(BuildProcess,
 	(@build_steps begin
 		GetSources(libvl)
 		@build_steps begin
-			FileRule(joinpath(usr, "lib", "libvl."*shlib_ext), @build_steps begin
-				MakeTargets(src, ["all"]; env = @compat Dict("MEX" => ""))
-				CreateDirectory(libdir(libvl))
-				`cp -a $src/bin/$arch/libvl.$shlib_ext $usr/lib`
-			end)
-		end
-	end), libvl, os = :Unix)
+    	FileRule(joinpath(usr, "lib", "libvl."*shlib_ext), @build_steps begin
+		  	MakeTargets(src, ["all"]; env = @compat Dict("MEX" => ""))
+		  	CreateDirectory(libdir(libvl))
+		  	`cp -a $src/bin/$arch/libvl.$shlib_ext $usr/lib`
+		end)
+	end
+end), libvl, os = :Unix)
+
+provides(BuildProcess,
+(@build_steps begin
+	GetSources(libvl)
+	@build_steps begin
+		FileRule(joinpath(usr, "lib", "vl."*shlib_ext), @build_steps begin
+			CreateDirectory(libdir(libvl))
+			`cp $src/bin/win64/vl.$shlib_ext $usr/lib`
+		end)
+	end
+end),libvl, os = :Windows)
+
 
 @BinDeps.install @compat Dict(:libvl => :libvl)
