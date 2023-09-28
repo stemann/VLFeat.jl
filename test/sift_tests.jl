8f9da2fdfda88f1fffaad54fdfd44888b2ae7acd
Jesper Stemann Andersen <jsa@ihp.dk>
2023-09-28 10:19:40 +0200

    Replaced lena with cameraman test image

--- test/sift_tests.jl
+++ test/sift_tests.jl
@@ -2,9 +2,9 @@ using Test
 using TestImages
 using VLFeat
 
-@testset "vl_sift with float descriptors on lena_gray_256" begin
-  lena = TestImages.testimage("lena_gray_256")
-  (f,d) = VLFeat.vl_sift(lena; floatDescriptors=true)
+@testset "vl_sift with float descriptors on cameraman image" begin
+  cameraman_image = TestImages.testimage("cameraman")
+  (f,d) = VLFeat.vl_sift(cameraman_image; floatDescriptors=true)
   @testset "Dimensions of features and descriptors" begin
     @test size(f,1) == 4
     @test size(d,1) == 128
